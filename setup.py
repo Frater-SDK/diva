c12d67b8c6b03d67cd9e2af4928b8fbac7088e4a
John Henning <john.l.henning@ibm.com>
2020-03-15 19:03:51 -0400

    added min python version

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup, find_packages
 
 packages = find_packages(exclude=['tests*'])
 dependencies = list(line.strip() for line in open('requirements.txt').readlines())
+
 setup(
     name='diva-frater',
-    version='0.1.4',
+    version='0.1.5',
     packages=packages,
     url='https://github.com/frater/diva',
     license='MIT',
     author='John Henning',
     author_email='john.l.henning@ibm.com',
     description='DIVA Frater library containing data types, base Component classes, and visualization tools',
-    install_requires=dependencies
+    install_requires=dependencies,
+    python_requires='>=3.6'
 )
